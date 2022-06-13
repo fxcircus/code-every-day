1dcc70bf78803f2de8493d5fa5c20bfe8f16c653
Roy Daniel <roydanielr@gmail.com>
2022-06-13 19:33:05 -0400

    first commit for plusMinus

---
+++ hacker_rank/01_plus_minus.js
@@ -0,0 +1,14 @@
+
+/*
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ * Calculate the ratios of its elements that are positive, negative, and zero.
+ * Print the decimal value of each fraction on a new line with 6 places after the decimal.
+ */
+
+function plusMinus(arr) {
+    console.log(arr)
+}
+
+plusMinus([1, 1, 0, -1, -1])
+
+
