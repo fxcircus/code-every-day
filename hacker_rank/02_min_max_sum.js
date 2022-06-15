1703d39d9a10999b921cc71e1de31854469d3c37
Roy Daniel <roydanielr@gmail.com>
2022-06-14 21:36:33 -0400

    solved

--- hacker_rank/02_min_max_sum.js
+++ hacker_rank/02_min_max_sum.js
@@ -1,16 +1,29 @@
 /*
-Input - and array of five positive integers
-output - print the minimum and maximum values that can be calculated by summing exactly four of the five integers.
-(a single line of two space-separated long integers.)
-
+Input - Array with 5 positive integers
+output - minimum and maximum values that can be calculated by summing exactly four of the five integers.
+(print in a single line of two space-separated long integers.)
 */
 
 
 
 function miniMaxSum(arr) {
-    // Write your code here
-    console.log(arr)
+    let minSum = 0
+    let maxSum = 0
+    arr.sort((a, b) => a - b)
+    // console.log(`sorted: ${arr}\n`) // print sorted array
+    arr.forEach((element, idx) => {
+        if(idx === 0) {
+            minSum += element
+        } else if(idx <= 3) {
+            minSum += element
+            maxSum += element
+        } else if(idx === 4) {
+            maxSum += element
+        }
+        // console.log(`idx = ${idx}\tminSum = ${minSum}\tmaxSum = ${maxSum}`) // print iteration result
+    })
+    console.log(`${minSum} ${maxSum}`)
 }
 
-const arr = [1,3, 5, 7, 9]
+const arr = [6, 3, 373, 23, 90]
 miniMaxSum(arr)
