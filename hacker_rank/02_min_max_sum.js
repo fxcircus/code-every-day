27f7f02f89db0767b9f46887a44af998e420018b
Roy Daniel <roydanielr@gmail.com>
2022-06-14 21:12:37 -0400

    minMaxSum initial setup

---
+++ hacker_rank/02_min_max_sum.js
@@ -0,0 +1,16 @@
+/*
+Input - and array of five positive integers
+output - print the minimum and maximum values that can be calculated by summing exactly four of the five integers.
+(a single line of two space-separated long integers.)
+
+*/
+
+
+
+function miniMaxSum(arr) {
+    // Write your code here
+    console.log(arr)
+}
+
+const arr = [1,3, 5, 7, 9]
+miniMaxSum(arr)
