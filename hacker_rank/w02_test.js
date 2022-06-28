856fd35730cd829db1a51f40352114b46c6b63ba
Roy Daniel <roydanielr@gmail.com>
2022-06-27 22:10:26 -0400

    week 2 test try 2

--- hacker_rank/w02_test.js
+++ hacker_rank/w02_test.js
@@ -5,72 +5,9 @@
  * The function accepts 2D_INTEGER_ARRAY matrix as parameter.
  */
 
-// function numbercompare(num1, num2) {
-//     return num2 - num1
-// }
-
-// MK I
-
-// function flippingMatrix(matrix) {
-//     let oneArr = []
-//     let len = 0, res = 0
-//     matrix.forEach(arr => {
-//         oneArr = oneArr.concat(arr)
-//     })
-//     console.log(oneArr)
-//     oneArr.sort(numbercompare)
-//     len = oneArr.length
-//     for(let i = Math.sqrt(len) - 1; i >= 0; i--) {
-//         // console.log(i, res)
-        
-//         res+=oneArr[i]
-//     }
-//     console.log(oneArr)
-//     // console.log(res)
-
-// }
-
-// MK II
-
-// function flippingMatrix(matrix) {
-//     let oneArr = []
-//     let len = matrix.length, res = 0
-//     matrix.forEach((arr, idx) => {
-//         arr.sort(numbercompare)
-//         // console.log(arr[idx])
-//         res += arr[0]
-//         console.log(arr[0])
-//     })
-//     return res
-// }
-
-// const flipPos = (arr1, i, arr2, j) => {
-//     const tmp = arr1[i]
-//     // console.log(arr1, i, arr2, j)
-//     arr1[i] = arr2[j]
-//     arr2[j] = tmp
-// }
-
-// MK III
-
-// function flippingMatrix(matrix) {
-
-//     let len = matrix.length - 1, res = 0
-
-//     for(let i = 0; i <= len; i++) {
-//         if(matrix[i][0] < matrix[i][len]) {
-//             flipPos(matrix[i], 0, matrix[i], len)
-//         }
-//         if(matrix[0][i] < matrix[len][i]) {
-//             flipPos(matrix[0], i, matrix[len],i)
-//         }
-//     }
-//     console.log(matrix)
-//     return res
-// }
-
-// MK IV
-
+// ------------------------------------
+// AUX functions:
+// ------------------------------------
 const flipRow = (arr) => {
     for(let i = 0, j = arr.length - 1; i < j; i++, j--) {
         const tmp = arr[i]
@@ -79,7 +16,6 @@ const flipRow = (arr) => {
     }
     return arr
 }
-
 const flipColumn = (multArr, rowNum) => {
     for(let i = 0, j = multArr.length - 1; i < j; i++, j--) {
         const tmp = multArr[i][rowNum]
@@ -88,21 +24,34 @@ const flipColumn = (multArr, rowNum) => {
     }
     return multArr
 }
-
+// ------------------------------------
+// Main: function:
+// ------------------------------------
 function flippingMatrix(matrix) {
-
     let len = matrix.length - 1, res = 0
 
+    // PT1: flip rows && columns
     for(let i = 0; i <= len; i++) {
         if(matrix[i][0] < matrix[i][len]) {
             flipRow(matrix[i])
         }
         if(matrix[0][i] < matrix[len][i]) {
-            // flipPos(matrix[0], i, matrix[len],i)
             flipColumn(matrix, i)
         }
     }
-    console.log(matrix)
+    // console.log(matrix)
+
+    // PT2: calculate inner matrix sum
+    const midPoint = len / 2
+    for(let i = 0; i <= midPoint; i++){
+        for(let j = 0; j <= midPoint; j++){
+            // console.log(matrix[i][j])
+            res+=matrix[i][j]
+        }
+    }
+
+
+    
     return res
 }
 
