b38dccc7246ce33bcdb8a8319cfa8f2c6962627c
Roy Daniel <roydanielr@gmail.com>
2022-07-02 09:17:16 -0400

    cleanup and test cases

--- hacker_rank/w03_ex02_permuting_two_arrays_2.js
+++ hacker_rank/w03_ex02_permuting_two_arrays_2.js
@@ -9,17 +9,14 @@
 function birthday(s, d, m) {
     let res = 0
     for (let i = 0; i < s.length; i++) {
-        let sum = 0
-        const segmentEnd = i + m
-        for(j = i; j < segmentEnd && segmentEnd <= s.length ; j++) {
-            sum += s[j]
-        }
-        console.log(`i = ${i}\tsum = ${sum}`)
-        if(sum === d) {
-            res++
-        }
+        let sum = 0, segmentEnd = i + m
+        for(let j = i; j < segmentEnd && segmentEnd <= s.length ; j++) sum += s[j] 
+        sum === d ? res++ : null
     }
     return res
 }
 
 console.log(birthday([2, 2, 1, 3, 2], 4, 2)) // 2
+console.log(birthday([1, 2, 1, 3, 2], 3, 2)) // 2
+console.log(birthday([1, 1, 1, 1, 1, 1], 3, 2)) // 0
+console.log(birthday([4], 4, 1)) // 1
