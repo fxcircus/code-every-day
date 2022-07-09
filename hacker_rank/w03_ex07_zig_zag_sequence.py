94b9af7c884b97a2ded4bb04a1d8d99ab540a394
Roy Daniel <roydanielr@gmail.com>
2022-07-09 12:02:00 -0400

    findZigZagSequence solution

--- hacker_rank/w03_ex07_zig_zag_sequence.py
+++ hacker_rank/w03_ex07_zig_zag_sequence.py
@@ -6,15 +6,15 @@ You cannot add or remove lines of code.
 
 def findZigZagSequence(a, n):
     a.sort()
-    mid = int((n + 1)/2)
+    mid = int(n /2) # 1
     a[mid], a[n-1] = a[n-1], a[mid]
 
     st = mid + 1
-    ed = n - 1
+    ed = n - 2 # 2
     while(st <= ed):
         a[st], a[ed] = a[ed], a[st]
         st = st + 1
-        ed = ed + 1
+        ed = ed - 1 # 3
 
     for i in range (n):
         if i == n-1:
@@ -23,7 +23,7 @@ def findZigZagSequence(a, n):
             print(a[i], end = ' ')
     return
 
-print(findZigZagSequence([2, 3, 5, 1, 4], 5))
+findZigZagSequence([2, 3, 5, 1, 4], 5) # 1 2 3 5 4
 
 
 
